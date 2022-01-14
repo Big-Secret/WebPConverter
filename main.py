8dbd1e76f943a1bdaba946afe683741916b65299
Big Secret <alex.nawabi@gmail.com>
2022-01-13 20:15:35 -0800

    my stuffs

--- main.py
+++ main.py
@@ -300,6 +300,33 @@ class RedditApp(QMainWindow):
         self.connectToReddit()
         self.createMultiReddit()
 
+        class ConvertImages():
+            def __init__(self, inputPath):
+                self.outputFolder = "D:\Projects\Crypto Priks\CreateWebP\webp"
+                self.inputFolder = inputPath
+
+                try:
+                    os.mkdir(self.outputFolder)
+                except:
+                    pass
+
+            def convertWebP(self, imageName):
+                imagePath = f"{self.inputFolder}\{imageName}"
+                imageName = imageName.split(".")[0]
+                print(f"Converting {imageName} from {imagePath}")
+                image = Image.open(imagePath)
+                image = image.convert("RGBA")
+                imgInfo = image.info
+                image.save(f"{self.outputFolder}\{imageName}.webp", format='webp', lossless=False, quality=80, method=6,
+                           **imgInfo)
+                print(f"{imageName} saved as {imageName}.webp @ {self.outputFolder}\{imageName}.webp")
+
+            def readDirectory(self):
+                self.dir = os.listdir(self.inputFolder)
+                for item in self.dir:
+                    if "." in item:
+                        self.convertWebP(item)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
